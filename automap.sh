5a3ae21ab8bebaa37f0852328e92bb8b8e51d0b3
Fabrice Aneche <akh@inair.space>
2020-03-12 11:40:17 -0400

    added automap generation

---
+++ automap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+MBTILESDIR=/opt/data/public_files
+
+make mbtilestokv kvtilesd grpc_health_probe
+
+mkdir -p build
+
+while IFS=";" read -r name maxzoom file lat lng 
+do
+      echo "starting $name $maxzoom $file $lat $lng"
+      rm -f "build/${name}.db"
+      ./cmd/mbtilestokv/mbtilestokv -tilesPath ${MBTILESDIR}/${file} -dbPath "build/${name}.db" -maxZoom $maxzoom -centerLat $lat -centerLng $lng 
+      cp "build/${name}.db" cmd/kvtilesd/map.db
+      docker build cmd/kvtilesd -t akhenakh/kvtiles:${name}-latest
+      docker push akhenakh/kvtiles:${name}-latest
+       
+done < <(egrep -v "^#" maps.csv)
+
+
